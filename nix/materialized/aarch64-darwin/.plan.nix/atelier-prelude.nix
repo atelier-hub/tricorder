0572746a257bb2617f18fa7ff8234446e7f57b61
cgeorgii <12649588+cgeorgii@users.noreply.github.com>
2026-06-03 22:28:33 +0000

    chore: update aarch64-darwin materialization

---
+++ nix/materialized/aarch64-darwin/.plan.nix/atelier-prelude.nix
@@ -0,0 +1,47 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  ({
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "atelier-prelude"; version = "0.1.0.0"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "cgeorgii@gmail.com";
+      author = "Christian Georgii";
+      homepage = "";
+      url = "";
+      synopsis = "Custom relude-based prelude with Effectful conventions";
+      description = "A custom prelude based on relude, adapted for Effectful — part of the atelier toolkit.";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [];
+      dataDir = ".";
+      dataFiles = [];
+      extraSrcFiles = [];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+    };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
+          (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+        ];
+        buildable = true;
+        modules = [ "Paths_atelier_prelude" ];
+        hsSourceDirs = [ "src" ];
+      };
+    };
+  } // rec { src = pkgs.lib.mkDefault ../atelier-prelude; }) // {
+    cabal-generator = "hpack";
+  }
--- nix/materialized/aarch64-darwin/.plan.nix/tricorder.nix
+++ nix/materialized/aarch64-darwin/.plan.nix/tricorder.nix
@@ -31,58 +31,19 @@
       extraDocFiles = [];
     };
     components = {
-      "library" = {
-        depends = [
-          (hsPkgs."Cabal-syntax" or (errorHandler.buildDepError "Cabal-syntax"))
-          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
-          (hsPkgs."ansi-terminal" or (errorHandler.buildDepError "ansi-terminal"))
-          (hsPkgs."tricorder".components.sublibs.atelier or (errorHandler.buildDepError "tricorder:atelier"))
-          (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
-          (hsPkgs."base" or (errorHandler.buildDepError "base"))
-          (hsPkgs."brick" or (errorHandler.buildDepError "brick"))
-          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
-          (hsPkgs."casing" or (errorHandler.buildDepError "casing"))
-          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
-          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
-          (hsPkgs."effectful" or (errorHandler.buildDepError "effectful"))
-          (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
-          (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
-          (hsPkgs."effectful-th" or (errorHandler.buildDepError "effectful-th"))
-          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
-          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
-          (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
-          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
-          (hsPkgs."network" or (errorHandler.buildDepError "network"))
-          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
-          (hsPkgs."process" or (errorHandler.buildDepError "process"))
-          (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
-          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
-          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
-          (hsPkgs."text" or (errorHandler.buildDepError "text"))
-          (hsPkgs."time" or (errorHandler.buildDepError "time"))
-          (hsPkgs."time-units" or (errorHandler.buildDepError "time-units"))
-          (hsPkgs."typed-process" or (errorHandler.buildDepError "typed-process"))
-          (hsPkgs."vty" or (errorHandler.buildDepError "vty"))
-          (hsPkgs."vty-crossplatform" or (errorHandler.buildDepError "vty-crossplatform"))
-          (hsPkgs."yaml" or (errorHandler.buildDepError "yaml"))
-        ];
-        buildable = true;
-        modules = [ "Paths_tricorder" ];
-        hsSourceDirs = [ "tricorder/src" ];
-      };
       sublibs = {
-        "atelier" = {
+        "tricorder-internal" = {
           depends = [
+            (hsPkgs."Cabal-syntax" or (errorHandler.buildDepError "Cabal-syntax"))
             (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
-            (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
+            (hsPkgs."ansi-terminal" or (errorHandler.buildDepError "ansi-terminal"))
+            (hsPkgs."atelier-core" or (errorHandler.buildDepError "atelier-core"))
+            (hsPkgs."atelier-prelude" or (errorHandler.buildDepError "atelier-prelude"))
             (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+            (hsPkgs."brick" or (errorHandler.buildDepError "brick"))
             (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
             (hsPkgs."casing" or (errorHandler.buildDepError "casing"))
             (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."contra-tracer" or (errorHandler.buildDepError "contra-tracer"))
-            (hsPkgs."daemons" or (errorHandler.buildDepError "daemons"))
             (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
             (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
             (hsPkgs."effectful" or (errorHandler.buildDepError "effectful"))
@@ -90,135 +51,63 @@
             (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
             (hsPkgs."effectful-th" or (errorHandler.buildDepError "effectful-th"))
             (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
-            (hsPkgs."fsnotify" or (errorHandler.buildDepError "fsnotify"))
-            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
-            (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
-            (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
-            (hsPkgs."hs-opentelemetry-api" or (errorHandler.buildDepError "hs-opentelemetry-api"))
-            (hsPkgs."hs-opentelemetry-exporter-otlp" or (errorHandler.buildDepError "hs-opentelemetry-exporter-otlp"))
-            (hsPkgs."hs-opentelemetry-sdk" or (errorHandler.buildDepError "hs-opentelemetry-sdk"))
-            (hsPkgs."http-api-data" or (errorHandler.buildDepError "http-api-data"))
-            (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
-            (hsPkgs."ki" or (errorHandler.buildDepError "ki"))
-            (hsPkgs."list-t" or (errorHandler.buildDepError "list-t"))
-            (hsPkgs."prometheus-client" or (errorHandler.buildDepError "prometheus-client"))
-            (hsPkgs."prometheus-metrics-ghc" or (errorHandler.buildDepError "prometheus-metrics-ghc"))
-            (hsPkgs."rel8" or (errorHandler.buildDepError "rel8"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
             (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
-            (hsPkgs."stm-containers" or (errorHandler.buildDepError "stm-containers"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
             (hsPkgs."text" or (errorHandler.buildDepError "text"))
             (hsPkgs."time" or (errorHandler.buildDepError "time"))
             (hsPkgs."time-units" or (errorHandler.buildDepError "time-units"))
-            (hsPkgs."unagi-chan" or (errorHandler.buildDepError "unagi-chan"))
-            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
-            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
-            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
-            (hsPkgs."wai" or (errorHandler.buildDepError "wai"))
-            (hsPkgs."warp" or (errorHandler.buildDepError "warp"))
+            (hsPkgs."typed-process" or (errorHandler.buildDepError "typed-process"))
+            (hsPkgs."vty" or (errorHandler.buildDepError "vty"))
+            (hsPkgs."vty-crossplatform" or (errorHandler.buildDepError "vty-crossplatform"))
+            (hsPkgs."yaml" or (errorHandler.buildDepError "yaml"))
           ];
           buildable = true;
           modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "atelier/src" ];
-        };
-        "atelier-prelude" = {
-          depends = [
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
-            (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
-            (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
-          ];
-          buildable = true;
-          modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "atelier/prelude" ];
-        };
-        "atelier-testing" = {
-          depends = [
-            (hsPkgs."tricorder".components.sublibs.atelier or (errorHandler.buildDepError "tricorder:atelier"))
-            (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
-            (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
-            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
-            (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
-            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
-            (hsPkgs."postgres-options" or (errorHandler.buildDepError "postgres-options"))
-            (hsPkgs."string-conversions" or (errorHandler.buildDepError "string-conversions"))
-            (hsPkgs."text" or (errorHandler.buildDepError "text"))
-            (hsPkgs."tmp-postgres" or (errorHandler.buildDepError "tmp-postgres"))
-            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
-            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
-          ];
-          buildable = true;
-          modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "atelier/testing" ];
+          hsSourceDirs = [ "src" ];
         };
       };
       exes = {
         "tricorder-daemon" = {
           depends = [
-            (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
+            (hsPkgs."atelier-prelude" or (errorHandler.buildDepError "atelier-prelude"))
             (hsPkgs."base" or (errorHandler.buildDepError "base"))
             (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
             (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
-            (hsPkgs."tricorder" or (errorHandler.buildDepError "tricorder"))
+            (hsPkgs."tricorder".components.sublibs.tricorder-internal or (errorHandler.buildDepError "tricorder:tricorder-internal"))
           ];
           buildable = true;
           modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "tricorder/daemon" ];
+          hsSourceDirs = [ "daemon" ];
           mainPath = [ "Main.hs" ];
         };
         "tricorder-exe" = {
           depends = [
-            (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
+            (hsPkgs."atelier-prelude" or (errorHandler.buildDepError "atelier-prelude"))
             (hsPkgs."base" or (errorHandler.buildDepError "base"))
             (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
             (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
-            (hsPkgs."tricorder" or (errorHandler.buildDepError "tricorder"))
+            (hsPkgs."tricorder".components.sublibs.tricorder-internal or (errorHandler.buildDepError "tricorder:tricorder-internal"))
           ];
           buildable = true;
           modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "tricorder/app" ];
+          hsSourceDirs = [ "app" ];
           mainPath = [ "Main.hs" ];
         };
       };
       tests = {
-        "atelier-test" = {
-          depends = [
-            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
-            (hsPkgs."async" or (errorHandler.buildDepError "async"))
-            (hsPkgs."tricorder".components.sublibs.atelier or (errorHandler.buildDepError "tricorder:atelier"))
-            (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
-            (hsPkgs."base" or (errorHandler.buildDepError "base"))
-            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
-            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
-            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
-            (hsPkgs."effectful" or (errorHandler.buildDepError "effectful"))
-            (hsPkgs."effectful-core" or (errorHandler.buildDepError "effectful-core"))
-            (hsPkgs."effectful-plugin" or (errorHandler.buildDepError "effectful-plugin"))
-            (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
-            (hsPkgs."hs-opentelemetry-api" or (errorHandler.buildDepError "hs-opentelemetry-api"))
-            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
-            (hsPkgs."hspec-hedgehog" or (errorHandler.buildDepError "hspec-hedgehog"))
-            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
-            (hsPkgs."stm-containers" or (errorHandler.buildDepError "stm-containers"))
-            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
-            (hsPkgs."tasty-hspec" or (errorHandler.buildDepError "tasty-hspec"))
-            (hsPkgs."time" or (errorHandler.buildDepError "time"))
-          ];
-          build-tools = [
-            (hsPkgs.pkgsBuildBuild.tasty-discover.components.exes.tasty-discover or (pkgs.pkgsBuildBuild.tasty-discover or (errorHandler.buildToolDepError "tasty-discover:tasty-discover")))
-          ];
-          buildable = true;
-          modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "atelier/test" ];
-          mainPath = [ "Driver.hs" ];
-        };
         "tricorder-test" = {
           depends = [
             (hsPkgs."Cabal-syntax" or (errorHandler.buildDepError "Cabal-syntax"))
             (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
-            (hsPkgs."tricorder".components.sublibs.atelier or (errorHandler.buildDepError "tricorder:atelier"))
-            (hsPkgs."tricorder".components.sublibs.atelier-prelude or (errorHandler.buildDepError "tricorder:atelier-prelude"))
+            (hsPkgs."atelier-core" or (errorHandler.buildDepError "atelier-core"))
+            (hsPkgs."atelier-prelude" or (errorHandler.buildDepError "atelier-prelude"))
             (hsPkgs."base" or (errorHandler.buildDepError "base"))
             (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
             (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
@@ -234,7 +123,7 @@
             (hsPkgs."text" or (errorHandler.buildDepError "text"))
             (hsPkgs."time" or (errorHandler.buildDepError "time"))
             (hsPkgs."time-units" or (errorHandler.buildDepError "time-units"))
-            (hsPkgs."tricorder" or (errorHandler.buildDepError "tricorder"))
+            (hsPkgs."tricorder".components.sublibs.tricorder-internal or (errorHandler.buildDepError "tricorder:tricorder-internal"))
             (hsPkgs."typed-process" or (errorHandler.buildDepError "typed-process"))
             (hsPkgs."unagi-chan" or (errorHandler.buildDepError "unagi-chan"))
           ];
@@ -243,9 +132,11 @@
           ];
           buildable = true;
           modules = [ "Paths_tricorder" ];
-          hsSourceDirs = [ "tricorder/test" ];
+          hsSourceDirs = [ "test" ];
           mainPath = [ "Driver.hs" ];
         };
       };
     };
-  } // rec { src = pkgs.lib.mkDefault ../.; }) // { cabal-generator = "hpack"; }
+  } // rec { src = pkgs.lib.mkDefault ../tricorder; }) // {
+    cabal-generator = "hpack";
+  }
