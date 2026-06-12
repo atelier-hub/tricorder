f2db8bc2c73a7abccf6bb936c7b6c3a5da1d838f
Victor Nascimento Bakke <gipphe@gmail.com>
2026-06-12 16:20:38 +0200

    chore: drop modules from materialized files
    Provide `package.yaml` files for the `haskell-plan-to-nix` stage of the
    builds so that the materialization uses `.cabal` files generated from
    `package.yaml` files in isolation, whereby `hpack` does not find any
    source modules to populate the `exposed-modules` list with.
    
    This simplifies materialization quite a bit, since adding a module will
    no longer cause every set of materialized files to have to be
    re-materialized. Materialization is a bit cumbersome since we have it
    for multiple architectures, and multiple GHC versions.

--- nix/package/nix-hpack.nix
+++ nix/package/nix-hpack.nix
@@ -21,29 +21,38 @@ writeShellApplication {
     }
 
     usage() {
-      info "Usage: nix-hpack [packages...]"
+      info "Usage: nix-hpack [--keep] [packages...]"
       info "Args:"
+      info "  --keep:    Keep the generated package.yaml file. By default, the generated"
+      info "             package.yaml file is deleted once hpack has generated the .cabal"
+      info "             file."
       info "  packages:  Path to a package directory containing a 'package.nix' to"
       info "             convert."
       info "             Defaults to converting every package under the current working"
       info "             directory."
     }
 
+    packages=()
+    keep_package_yaml=false
     for arg in "$@"; do
       case "$arg" in
         --help|-h)
           usage
           exit 0
           ;;
+        --keep)
+          keep_package_yaml=true
+          ;;
+        *)
+          packages+=("$arg")
+          ;;
       esac
     done
-
-    packages=("$@")
 
     if [ ''${#packages[@]} -eq 0 ]; then
       # If no packages are specified, find all packages in the current
       # directory.
-      mapfile -t packages < <(find . -type f -name "package.nix" -printf "%h\n")
+      mapfile -t packages < <(find "$PWD" -type f -name "package.nix" -printf "%h\n")
     fi
 
     for package in "''${packages[@]}"; do
@@ -60,14 +69,16 @@ writeShellApplication {
       # nix-instantiate tries to initialize /nix/var/nix on startup, which fails
       # in a sandbox. Repackageect to a writable tmpdir for pure --eval use.
       nix_state=$(mktemp -d)
-      export NIX_STATE_package="$nix_state"
-      export NIX_LOG_package="$nix_state/log"
-
+      export NIX_STATE_DIR="$nix_state"
+      export NIX_LOG_DIR="$nix_state/log"
       nix-instantiate --strict --json --eval "$source" \
         | yq --prettyPrint --input-format json --output-format yaml >> "$temp_yaml"
+      rm -rf "$nix_state"
 
       hpack --silent "$temp_yaml" &>/dev/null
-      rm -rf "$temp_yaml" "$nix_state"
+      if test "$keep_package_yaml" = "false"; then
+        rm -rf "$temp_yaml"
+      fi
     done
   '';
 }
