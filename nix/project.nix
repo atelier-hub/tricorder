2b167c196e8cf51aac7b53652f4e15f6fd25cdff
Victor Nascimento Bakke <gipphe@gmail.com>
2026-06-12 16:20:38 +0200

    chore: drop modules from materialized files
    Provide `package.yaml` files for the `haskell-plan-to-nix` stage of the
    builds so that the materialization uses `.cabal` files generated from
    `package.yaml` files in isolation, whereby `hpack` does not find any
    source modules to populate the `exposed-modules` list with.
    
    This simplifies materialization quite a bit, since adding a module will
    no longer cause every set of materialized files to have to be
    re-materialized. Materialization is a bit cumbersome since we have it
    for multiple architectures, and multiple GHC versions.

--- nix/project.nix
+++ nix/project.nix
@@ -4,12 +4,28 @@
   compiler-nix-name,
   self,
 }:
+let
+  nix-hpack = pkgs.callPackage ./package/nix-hpack.nix { };
+  # Include the `package.yaml` file for the `haskell-plan-to-nix` step of the
+  # build process. When `haskell-plan-to-nix` uses `package.yaml` instead of
+  # the raw `.cabal` files, it omits the module paths from the materialized
+  # files. By keeping module paths out of our materialized files, we don't have
+  # to update materialization for every module we add or remove, only for
+  # dependencies.
+  src = pkgs.runCommand "src" { } ''
+    mkdir -p src
+    cp -r ${./..}/* src
+    chmod -R +w src
+    ls -la src
+    (cd src && ${nix-hpack}/bin/nix-hpack --keep)
+    mv src $out
+  '';
+in
 pkgs.haskell-nix.cabalProject' {
-  src = ../.;
-  inherit compiler-nix-name;
+  inherit src compiler-nix-name;
 
   # Enable materialization for deterministic builds and better CI caching
-  materialized = ./materialized + "/${pkgs.stdenv.hostPlatform.system}/${compiler-nix-name}";
+  materialized = ./materialized/${pkgs.stdenv.hostPlatform.system}/${compiler-nix-name};
   checkMaterialization = true;
 
   # Add tmp-postgres from flake input
