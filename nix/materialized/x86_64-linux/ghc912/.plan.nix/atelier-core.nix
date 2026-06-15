86a93bd1b271b3dd7856d867e5078f3185e41e20
Victor Nascimento Bakke <victor.bakke@tweag.io>
2026-06-15 13:59:07 +0200

    chore: drop modules from materialized files (#196)
    Provide `package.yaml` files for the `haskell-plan-to-nix` stage of the
    builds so that the materialization uses `.cabal` files generated from
    `package.yaml` files in isolation, whereby `hpack` does not find any
    source modules to populate the `exposed-modules` list with.
    
    This simplifies materialization quite a bit, since adding a module will
    no longer cause every set of materialized files to have to be
    re-materialized. Materialization is a bit cumbersome since we have it
    for multiple architectures, and multiple GHC versions.
    
    Depends on https://github.com/atelier-hub/tricorder/pull/201

--- nix/materialized/x86_64-linux/ghc912/.plan.nix/atelier-core.nix
+++ nix/materialized/x86_64-linux/ghc912/.plan.nix/atelier-core.nix
@@ -7,7 +7,7 @@
   , errorHandler
   , config
   , ... }:
-  {
+  ({
     flags = {};
     package = {
       specVersion = "2.0";
@@ -75,54 +75,7 @@
           (hsPkgs."warp" or (errorHandler.buildDepError "warp"))
         ];
         buildable = true;
-        modules = [
-          "Paths_atelier_core"
-          "Atelier/Component"
-          "Atelier/Config"
-          "Atelier/Effects/Arguments"
-          "Atelier/Effects/Await"
-          "Atelier/Effects/Cache"
-          "Atelier/Effects/Cache/Config"
-          "Atelier/Effects/Cache/Singleflight"
-          "Atelier/Effects/Chan"
-          "Atelier/Effects/Clock"
-          "Atelier/Effects/Conc"
-          "Atelier/Effects/Conc/Traced"
-          "Atelier/Effects/Console"
-          "Atelier/Effects/Debounce"
-          "Atelier/Effects/Delay"
-          "Atelier/Effects/Env"
-          "Atelier/Effects/Exit"
-          "Atelier/Effects/File"
-          "Atelier/Effects/FileSystem"
-          "Atelier/Effects/FileWatcher"
-          "Atelier/Effects/Input"
-          "Atelier/Effects/Internal/Coroutine"
-          "Atelier/Effects/Iterator"
-          "Atelier/Effects/Log"
-          "Atelier/Effects/Monitoring/Metrics"
-          "Atelier/Effects/Monitoring/Metrics/Registry"
-          "Atelier/Effects/Monitoring/Metrics/Server"
-          "Atelier/Effects/Monitoring/Tracing"
-          "Atelier/Effects/Monitoring/Tracing/Provider"
-          "Atelier/Effects/Posix/Daemons"
-          "Atelier/Effects/Posix/IO"
-          "Atelier/Effects/Process"
-          "Atelier/Effects/Publishing"
-          "Atelier/Effects/Tally"
-          "Atelier/Effects/Timeout"
-          "Atelier/Effects/UUID"
-          "Atelier/Effects/Yield"
-          "Atelier/Exception"
-          "Atelier/Time"
-          "Atelier/Types/Base64"
-          "Atelier/Types/HttpApiDataReadShow"
-          "Atelier/Types/JsonReadShow"
-          "Atelier/Types/QuietSnake"
-          "Atelier/Types/Semaphore"
-          "Atelier/Types/Semaphore/STM"
-          "Atelier/Types/WithDefaults"
-        ];
+        modules = [ "Paths_atelier_core" ];
         hsSourceDirs = [ "src" ];
       };
       tests = {
@@ -153,32 +106,12 @@
             (hsPkgs.pkgsBuildBuild.tasty-discover.components.exes.tasty-discover or (pkgs.pkgsBuildBuild.tasty-discover or (errorHandler.buildToolDepError "tasty-discover:tasty-discover")))
           ];
           buildable = true;
-          modules = [
-            "Unit/Atelier/ConfigSpec"
-            "Unit/Atelier/Effects/AwaitSpec"
-            "Unit/Atelier/Effects/Cache/SingleflightSpec"
-            "Unit/Atelier/Effects/CacheSpec"
-            "Unit/Atelier/Effects/ChanSpec"
-            "Unit/Atelier/Effects/Conc/TeardownStressSpec"
-            "Unit/Atelier/Effects/Conc/TracedSpec"
-            "Unit/Atelier/Effects/ConcSpec"
-            "Unit/Atelier/Effects/ConsoleSpec"
-            "Unit/Atelier/Effects/DebounceSpec"
-            "Unit/Atelier/Effects/FileSystemSpec"
-            "Unit/Atelier/Effects/FileWatcherSpec"
-            "Unit/Atelier/Effects/IteratorSpec"
-            "Unit/Atelier/Effects/LogSpec"
-            "Unit/Atelier/Effects/PublishingSpec"
-            "Unit/Atelier/Effects/TallySpec"
-            "Unit/Atelier/Effects/YieldSpec"
-            "Unit/Atelier/Types/Semaphore/STMSpec"
-            "Unit/Atelier/Types/SemaphoreSpec"
-            "Unit/Atelier/Types/WithDefaultsSpec"
-            "Paths_atelier_core"
-          ];
+          modules = [ "Paths_atelier_core" ];
           hsSourceDirs = [ "test" ];
           mainPath = [ "Driver.hs" ];
         };
       };
     };
-  } // rec { src = pkgs.lib.mkDefault ../atelier-core; }
+  } // rec { src = pkgs.lib.mkDefault ../atelier-core; }) // {
+    cabal-generator = "hpack";
+  }
